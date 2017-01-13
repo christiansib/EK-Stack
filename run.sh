845216b553a7380c64b678cf615e971f41f66f90
Felix Ortmann <0ortmann@informatik.uni-hamburg.de>
2017-01-13 01:27:42 +0100

    build on run

--- run.sh
+++ run.sh
@@ -4,4 +4,4 @@ echo "Rising the maximum memory that virtual machines are allowed to map"
 sudo sysctl -w vm.max_map_count=262144
 
 echo "Starting cim ..."
-docker-compose up
+docker-compose up --build
